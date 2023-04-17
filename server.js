21657fddd057872e760c4d9ada7d73e855830dca
Musa <musamaxwell15@gmail.com>
2023-04-17 23:56:40 +0300

    Final Project

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const Product = require('./models/productModel');
 const port = 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -40,10 +41,34 @@ app.put('/products/:id', async(req, res) => {
     try{
 
         const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        // we cannot find product in db
+        if(!product){
+            return res.status(404).json({message: `Product with id ${id} not found`});
+        }
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json({updatedProduct});
         
 
     }
     catch(error){
+        console.log(error.message);
+        res.status(500).json({message: error.message});
+    }
+});
+
+
+//delete a product
+
+app.delete('/products/:id', async(req, res) => {
+    try{
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if(!product){
+            return res.status(404).json({message: `Product with id ${id} not found`});
+        }
+        res.status(200).json({product});
+    }catch(error){
         console.log(error.message);
         res.status(500).json({message: error.message});
     }
